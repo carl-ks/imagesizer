60837bc00338f12b8d1784560333c64b95339a80
LuftHANDsa <156071622+carl-ks@users.noreply.github.com>
2026-06-10 13:57:10 +0200

    Add black formatter and docstring

--- src/imagesizer/compress_to_size.py
+++ src/imagesizer/compress_to_size.py
@@ -9,25 +9,51 @@ BYTES_CONVERSION = {
     "byte": 1,
 }
 
+
 def get_file_size(file):
+    """Returns the file size in bytes"""
     file.seek(0, 2)
     filesize = file.tell()
     file.seek(0)
     return filesize
 
-def compress_to_size(input_image_path, output_image_path, max_size, unit="mb", compression_steps = 5):
+
+def compress_to_size(
+    input_image_path, output_image_path, max_size, unit="mb", compression_steps=5
+):
+    """
+    Compresses image to specified maximum size.
+
+    Args:
+        input_image_path: Path to input image.
+        output_image_path: Path to output image.
+        max_size: Maximum size of output image (default in mb).
+        unit: Unit for max_size (default is mb). Options are 'gb', 'mb', 'kb', 'byte'.
+        compression_steps: Compression steps to take per iteration (integer, default 5).
+            Larger steps are faster but might overcompress. Max 75 for non-PNG image, max 8 for PNG.
+
+    Raises:
+        ValueError: If unit is invalid, max_size <= 0, or image cannot be
+            compressed to target size.
+    """
 
     if unit not in BYTES_CONVERSION:
-        raise ValueError(f"Invalid unit '{unit}'. Must be one of: {list(BYTES_CONVERSION.keys())}")
+        raise ValueError(
+            f"Invalid unit '{unit}'. Must be one of: {list(BYTES_CONVERSION.keys())}"
+        )
 
     if max_size <= 0:
         raise ValueError(f"Invalid max_size '{max_size}. Must be larger that 0.'")
 
     if not isinstance(compression_steps, int):
-        raise ValueError(f"Invalid compression_steps '{compression_steps}. Must be an integer.'")
+        raise ValueError(
+            f"Invalid compression_steps '{compression_steps}. Must be an integer.'"
+        )
 
     if compression_steps <= 0:
-        raise ValueError(f"Invalid compression_steps '{compression_steps}. Must be larger that 0.'")
+        raise ValueError(
+            f"Invalid compression_steps '{compression_steps}. Must be larger that 0.'"
+        )
 
     max_size_bytes = max_size * BYTES_CONVERSION[unit]
 
@@ -36,10 +62,14 @@ def compress_to_size(input_image_path, output_image_path, max_size, unit="mb", c
             im.verify()
             file_format = im.format
     except Exception as e:
-        raise ValueError(f"Exception: Uploaded file is broken or not an image. Details: {e}")
+        raise ValueError(
+            f"Exception: Uploaded file is broken or not an image. Details: {e}"
+        )
 
     if file_format.lower() == "png" and compression_steps >= 9:
-        raise ValueError(f"Invalid compression_steps '{compression_steps}. Compression steps for png must be lower or equal to 8.")  
+        raise ValueError(
+            f"Invalid compression_steps '{compression_steps}. Compression steps for png must be lower or equal to 8."
+        )
 
     with open(input_image_path, "rb") as im:
         original_filesize = get_file_size(im)
@@ -47,8 +77,8 @@ def compress_to_size(input_image_path, output_image_path, max_size, unit="mb", c
     filesize = original_filesize
 
     if filesize <= max_size_bytes:
-        shutil.copyfile(input_image_path, output_image_path)          
-        
+        shutil.copyfile(input_image_path, output_image_path)
+
     else:
         with Image.open(input_image_path) as im:
 
@@ -66,7 +96,7 @@ def compress_to_size(input_image_path, output_image_path, max_size, unit="mb", c
                     quality_level -= compression_steps
                 else:
                     raise ValueError("Could not compress image below max_size_bytes")
-        
+
                 im.save(output_image_path, format=file_format, quality=quality_level)
 
             else:
@@ -74,7 +104,12 @@ def compress_to_size(input_image_path, output_image_path, max_size, unit="mb", c
                 max_compression = 9
                 while compression_level <= max_compression:
                     byte_arr = io.BytesIO()
-                    im.save(byte_arr, format=file_format, compress_level=compression_level, optimize=True)
+                    im.save(
+                        byte_arr,
+                        format=file_format,
+                        compress_level=compression_level,
+                        optimize=True,
+                    )
                     filesize = get_file_size(byte_arr)
 
                     if filesize <= max_size_bytes:
@@ -82,6 +117,13 @@ def compress_to_size(input_image_path, output_image_path, max_size, unit="mb", c
 
                     compression_level += compression_steps
                 else:
-                    raise ValueError("Could not compress png image below max_size_bytes")
+                    raise ValueError(
+                        "Could not compress png image below max_size_bytes"
+                    )
 
-                im.save(output_image_path, format=file_format, compress_level=compression_level, optimize=True)
+                im.save(
+                    output_image_path,
+                    format=file_format,
+                    compress_level=compression_level,
+                    optimize=True,
+                )
