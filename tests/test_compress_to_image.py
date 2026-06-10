0567f5904277d69d03ecc5e8d51b465c853b0d1c
LuftHANDsa <156071622+carl-ks@users.noreply.github.com>
2026-06-10 14:01:24 +0200

    Rename and reformat tests

---
+++ tests/test_compress_to_image.py
@@ -0,0 +1,90 @@
+from imagesizer import compress_to_size
+
+import pytest
+import io
+import tempfile
+from PIL import Image
+
+def test_jpeg():        
+    output_path = io.BytesIO()
+    compress_to_size("tests/images/test_image.jpg", output_path, max_size=1)
+    output_size = output_path.tell()
+    assert output_size <= 1_000_000
+
+def test_jpeg_kb():        
+    output_path = io.BytesIO()
+    compress_to_size("tests/images/test_image.jpg", output_path, max_size=1_000, unit="kb")
+    output_size = output_path.tell()
+    assert output_size <= 1_000_000
+
+def test_jpeg_small_compression():        
+    output_path = io.BytesIO()
+    compress_to_size("tests/images/test_image.jpg", output_path, max_size=1, compression_steps=1)
+    output_size = output_path.tell()
+    assert output_size <= 1_000_000
+
+def test_jpeg_large_compression():        
+    output_path = io.BytesIO()
+    compress_to_size("tests/images/test_image.jpg", output_path, max_size=1, compression_steps=25)
+    output_size = output_path.tell()
+    assert output_size <= 1_000_000
+
+def test_jpeg_no_compression():        
+    output_file = tempfile.NamedTemporaryFile()
+    compress_to_size("tests/images/test_image.jpg", output_file.name, max_size=10)
+    output_size = output_file.tell()
+    assert output_size <= 10_000_000
+
+def test_png():        
+    output_path = io.BytesIO()
+    compress_to_size("tests/images/test_image.png", output_path, max_size=18)
+    output_size = output_path.tell()
+    assert output_size <= 18_000_000
+
+def test_webp():        
+    output_path = io.BytesIO()
+    compress_to_size("tests/images/test_image.webp", output_path, max_size=1)
+    output_size = output_path.tell()
+    assert output_size <= 1_000_000
+
+def test_no_image():
+    output_path = io.BytesIO()
+    with pytest.raises(ValueError):
+        compress_to_size("tests/images/test_image.pdf", output_path, max_size=1)
+
+    with pytest.raises(ValueError):
+        compress_to_size("tests/images/test_image.txt", output_path, max_size=1)
+
+def test_wrong_unit():
+    output_path = io.BytesIO()
+    with pytest.raises(ValueError):
+        compress_to_size("tests/images/test_image.jpg", output_path, max_size=1, unit="megabyte")
+
+def test_max_size_not_possible():
+    output_path = io.BytesIO()
+    with pytest.raises(ValueError):
+        compress_to_size("tests/images/test_image.jpg", output_path, max_size=0.01)
+
+    with pytest.raises(ValueError):
+        compress_to_size("tests/images/test_image.jpg", output_path, max_size=0)
+
+    with pytest.raises(ValueError):
+        compress_to_size("tests/images/test_image.jpg", output_path, max_size=-1)
+
+def test_compression_steps():
+    output_path = io.BytesIO()
+    with pytest.raises(ValueError):
+        compress_to_size("tests/images/test_image.jpg", output_path, max_size=1, compression_steps=5.5)
+
+    with pytest.raises(ValueError):
+            compress_to_size("tests/images/test_image.jpg", output_path, max_size=1, compression_steps=-1)
+
+    with pytest.raises(ValueError):
+            compress_to_size("tests/images/test_image.jpg", output_path, max_size=1, compression_steps=0)
+
+def test_output_is_image():
+    output_path = io.BytesIO()
+    compress_to_size("tests/images/test_image.jpg", output_path, max_size=1)
+    with Image.open(output_path) as im:
+            assert im.size[0] > 0
+            assert im.size[1] > 0
